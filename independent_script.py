057d2d79d795d4eba380d9639e19581a1d969db3
Mikkelando <ms.m.kl@mail.ru>
2024-06-18 20:03:03 +0300

    changed cv2 to imagio, added output for 'bad' cases

--- independent_script.py
+++ independent_script.py
@@ -1,21 +1,18 @@
 from argparse import ArgumentParser
-# from matplotlib import pyplot as plt
+import os
+import imageio
 import numpy as np
 from tqdm import tqdm
 from scipy.spatial import ConvexHull
-
 from eyes import Eye_Checker
-
-# import imageio.v2 as imageio
 import cv2
-# from skimage.transform import resize
-# from PIL import Image
+
 
 def find_n_best_frames(source, driving, n=1, cpu=False, mouth=0):
     # import face_alignment
 
     print('CREATING eye_checker entity...')
-    eye_checker = Eye_Checker(r'eyes\data\model_weights.pkl')
+    eye_checker = Eye_Checker('eyes/data/model_weights.pkl')
     print('=== eye_checker created ===')
     
     def normalize_kp(kp):
@@ -86,8 +83,10 @@ def find_n_best_frames(source, driving, n=1, cpu=False, mouth=0):
 def find_n_best_frames_in_source_video(source, driving, n=1, mouth=0):
     # import face_alignment
 
+    
+
     print('CREATING eye_checker entity...')
-    eye_checker = Eye_Checker('eyes\data\model_weights.pkl')
+    eye_checker = Eye_Checker('eyes/data/model_weights.pkl')
     print('=== eye_checker created ===')
     
     def normalize_kp(kp):
@@ -103,12 +102,24 @@ def find_n_best_frames_in_source_video(source, driving, n=1, mouth=0):
     source_kps = []
     source_scores = []
     source_candidates = []
+
+    source_kps_bad = []
+    source_scores_bad = []
+    source_candidates_bad = []   
     for i, image in tqdm(enumerate(source)):
 
         eye_score = eye_checker.check_eye(image)
+        mouth_status = eye_checker.check_mouth(image)
+        
+        source_candidates_bad.append(i)
+        source_scores_bad.append([eye_score])
+        kp_source_bad = eye_checker.get_kp(image)
+        kp_source_bad = normalize_kp(kp_source_bad)
+        source_kps_bad.append(kp_source_bad)
+
         if eye_score > 0.5:
             if mouth != 0:
-                mouth_status = eye_checker.check_mouth(image)
+                
                 if mouth_status is None:
                     continue
 
@@ -134,6 +145,8 @@ def find_n_best_frames_in_source_video(source, driving, n=1, mouth=0):
                     kp_source = normalize_kp(kp_source)
                     source_kps.append(kp_source)
 
+                    
+        
 
 
             else:
@@ -146,17 +159,41 @@ def find_n_best_frames_in_source_video(source, driving, n=1, mouth=0):
                 kp_source = normalize_kp(kp_source)
                 source_kps.append(kp_source)
 
+                
+       
+            
 
+    print('LEN OF SOURCE CANDIDATES : ', len(source_candidates))
+
+    # print('CREATING eye_checker_2 entity...')
+    # eye_checker_2 = Eye_Checker('eyes/data/model_weights.pkl')
+    # print('=== eye_checker_2 created ===')
 
     driving_kps = []
     driving_scores = []
     driving_candidates = []
+
+    driving_kps_bad = []
+    driving_scores_bad = []
+    driving_candidates_bad = []
+
+    
+
+    print('TOTAL LEN OF DRIVING: ', len(driving))
     for i, image in tqdm(enumerate(driving)):
 
         eye_score = eye_checker.check_eye(image)
+        mouth_status = eye_checker.check_mouth(image)
+
+        driving_candidates_bad.append(i)
+        driving_scores_bad.append([eye_score])
+        kp_driving_bad = eye_checker.get_kp(image)
+        kp_driving_bad = normalize_kp(kp_driving_bad)
+        driving_kps_bad.append(kp_driving_bad)
+        # print(f'eye score {i}/{len(driving)} . . . {eye_score} ')
         if eye_score > 0.5:
             if mouth != 0:
-                mouth_status = eye_checker.check_mouth(image)
+                
                 if mouth_status is None:
                     continue
 
@@ -198,7 +235,7 @@ def find_n_best_frames_in_source_video(source, driving, n=1, mouth=0):
 
 
 
-
+    print('LEN OF DRIVING CANDIDATES : ', len(driving_candidates))
 
 
 
@@ -208,31 +245,59 @@ def find_n_best_frames_in_source_video(source, driving, n=1, mouth=0):
     
 
 
+    if len(driving_candidates) > 0 and len(source_candidates) > 0:
+
+        for i, dr_idx in enumerate(driving_candidates):
+            for j, src_idx in enumerate(source_candidates):
+                new_norm = (np.abs(source_kps[j] - driving_kps[i]) ** 2).sum()
+
+                # if src_idx not in frame_nums:
+                #     norms.append(new_norm)
+                #     frame_nums.append(src_idx)
+                # else:
+
+                norms[src_idx] = new_norm
 
 
-    for i, dr_idx in enumerate(driving_candidates):
-        for j, src_idx in enumerate(source_candidates):
-            new_norm = (np.abs(source_kps[j] - driving_kps[i]) ** 2).sum()
-
-            # if src_idx not in frame_nums:
-            #     norms.append(new_norm)
-            #     frame_nums.append(src_idx)
-            # else:
-
-            norms[src_idx] = new_norm
 
 
+        # sorted_frames = [x for _, x in sorted(zip(norms, frame_nums))]
+        
+        if len(norms) == 0:
+            return None
+        result = sorted(norms, key=norms.get)
 
-
-    # sorted_frames = [x for _, x in sorted(zip(norms, frame_nums))]
+        
+        # Возвращаем список n лучших кадров
+        return result[:n]
     
-    if len(norms) == 0:
-        return None
-    result = sorted(norms, key=norms.get)
 
-    
-    # Возвращаем список n лучших кадров
-    return result[:n]
+    else:
+        for i, dr_idx in enumerate(driving_candidates_bad):
+            for j, src_idx in enumerate(source_candidates_bad):
+                new_norm = (np.abs(source_kps_bad[j] - driving_kps_bad[i]) ** 2).sum()
+
+                # if src_idx not in frame_nums:
+                #     norms.append(new_norm)
+                #     frame_nums.append(src_idx)
+                # else:
+
+                norms[src_idx] = new_norm
+
+
+
+
+        # sorted_frames = [x for _, x in sorted(zip(norms, frame_nums))]
+        
+        if len(norms) == 0:
+            return None
+        result = sorted(norms, key=norms.get)
+
+        
+        # Возвращаем список n лучших кадров
+        return result[:n]
+
+
 
 
 
@@ -251,29 +316,58 @@ def resize_image(image_path, target_size=(256, 256)):
 
 
 
+'''
+
+
+
+# def process_video(video_path, target_size=(256, 256)):
+#     # Загрузка видео
+#     cap = cv2.VideoCapture(video_path)
+#     frames = []
+    
+#     if not cap.isOpened():
+#         print(f"{'ERROR '*10} : {video_path}")
+#         return frames
+#     # Чтение и обработка каждого кадра
+#     while True:
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+        
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#         # Изменение размера кадра
+#         # resized_frame = cv2.resize(frame, target_size)
+        
+#         # Преобразование кадра в формат numpy array и добавление в список
+#         frames.append(np.array(frame))
+        
+#     cap.release()
+#     cv2.destroyAllWindows()
+#     print(f'{os.path.basename(video_path)} . . . done!')
+#     return frames
+
+'''
 
 
 def process_video(video_path, target_size=(256, 256)):
     # Загрузка видео
-    cap = cv2.VideoCapture(video_path)
+    try:
+        reader = imageio.get_reader(video_path)
+    except FileNotFoundError:
+        print(f"{'ERROR '*10} : {video_path}")
+        return []
+
     frames = []
-    
-    # Чтение и обработка каждого кадра
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if not ret:
-            break
-        
+    for frame in reader:
         # Изменение размера кадра
         # resized_frame = cv2.resize(frame, target_size)
         
         # Преобразование кадра в формат numpy array и добавление в список
         frames.append(np.array(frame))
         
-    cap.release()
-    
+    print(f'{os.path.basename(video_path)} . . . done!')
     return frames
-
 
 
 
@@ -281,7 +375,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     
 
-    # parser.add_argument("--source_image", default='' help="path to source image")
+   
     parser.add_argument("--source_video", help="path to source video")
     parser.add_argument("--driving_video", help="path to driving video")
     
@@ -295,7 +389,7 @@ if __name__ == "__main__":
 
 
  
-    # parser.add_argument("--cpu", dest="cpu", action="store_true", help="cpu mode.")
+   
  
 
 
@@ -305,82 +399,28 @@ if __name__ == "__main__":
         print('--n parametr must be at least 1')
 
     
+    
+   
     print('reading source...')
-    # source_image_sk = imageio.imread(opt.source_image)
-    # source_img = cv2.imread(opt.source_image)
-    
-    
-    print('resizing...')
-    # source_image_sk = np.array(resize(source_image_sk, (256, 256))[..., :3])
-    # source_image_sk = (source_image_sk * 255).astype(np.uint8)
-    # print('SHAPEEE source_image_sk :',source_image_sk.shape)
-    # print('SHAPEEE source_image_sk[0] :', source_image_sk[0].shape)
-
-    # source_image_cv2 = resize_image(opt.source_image)
-    # print(source_image_cv2.shape)
 
     source_video = process_video(opt.source_video)
-
-    # plt.imshow(source_image_cv2[0:256, 0:256, 0])
-    # plt.show()
-
-    # print(source_image_sk[0:256, 0:256, 0])
-    # resized_frame = np.uint16(cv2.resize(source_img, (256, 256)))
-    # print(source_img.shape)
-    # print(source_image.shape)
-    # source_image = source_img
-    # source_image = cv2.cvtColor(source_image, cv2.COLOR_BGR2GRAY)
-    
-    # cv2.imshow('source', source_image)
+    # cv2.destroyAllWindows()
+ 
 
 
-
-    # base_width = 256
-    # img = Image.open(opt.source_image)
-    # wpercent = (base_width / float(img.size[0]))
-    # hsize = int((float(img.size[1]) * float(wpercent)))
-    # img = img.resize((256, 256), Image.Resampling.LANCZOS)
-    # img.save('somepic.png')
-
-    # print(np.array(img).shape)
-
-    # source_image = source_image_sk
-    # source_image = source_image_cv2
 
 
 
 
     print('reading driving...')
-    # reader = imageio.get_reader(opt.driving_video)
-    # fps = reader.get_meta_data()['fps']
-    # driving_video = []
-    # try:
-    #     for im in reader:
-    #         driving_video.append(im)
-    # except RuntimeError:
-    #     pass
-    # reader.close()
-
-    # tmp = []
-    # for i, frame in enumerate(driving_video):
-    #     tmp.append(np.array(resize(frame, (256, 256))[..., :3])) 
-    #     print(i)
-    # # generator, kp_detector = load_checkpoints(config_path=opt.config, checkpoint_path=opt.checkpoint, cpu=opt.cpu)
-    # driving_video = tmp
-   
-
-
-
-
-
-
-
     driving_video = process_video(opt.driving_video)
 
 
-    print('params:')
+    print('\nparams:')
     print('n: ', int(opt.n))
-    print('n: ', opt.open_mouth)
+    print('mouth: ', opt.open_mouth)
+    print('len source: ', len(source_video))
+    print('len driving: ', len(driving_video))
     print()
 
     # list_of_best_frames = find_n_best_frames(source_image, driving_video, cpu=opt.cpu, n=int(opt.n), mouth = opt.open_mouth)
@@ -393,6 +433,11 @@ if __name__ == "__main__":
         print('LIST OF BEST FRAMES:  ') 
         print(*list_of_best_frames)
     print ("Best frame: " + str(i))
+    print('saving to txt . . .', end = '')
+    with open(f'best_frames_{os.path.basename(opt.source_video)[0]}_{os.path.basename(opt.driving_video)[0]}.txt', 'w') as file:
+        for el in list_of_best_frames:
+            file.write(str(el))
+    print(' OK')
     
     
 
